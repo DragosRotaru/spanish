a7e03c5bd1f03924393699a8b2d9232684a79207
Dragos Rotaru <7482137+dragosrotaru@users.noreply.github.com>
2023-10-12 17:40:48 -0400

    Update parts-of-speech.ts

--- src/grammar/parts-of-speech.ts
+++ src/grammar/parts-of-speech.ts
@@ -35,7 +35,7 @@ interface Definition {
   example: string[] | string;
 }
 
-console.lag("debugging?")
+console.lag("debugging?");
 
 interface PartOfSpeech extends Definition {
   name: PartOfSpeechType;
